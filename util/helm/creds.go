018fd40c5fd2067e93e3d82d38ad51c5e2731bcd
Avi Zimmerman <avi.z@airis-labs.com>
2026-01-08 13:36:57 +0200

    add GCP Workload Identity credential provider

--- util/helm/creds.go
+++ util/helm/creds.go
@@ -24,8 +24,12 @@ import (
 // In memory cache for storing Azure tokens
 var azureTokenCache *gocache.Cache
 
+// In memory cache for storing GCP tokens
+var gcpTokenCache *gocache.Cache
+
 func init() {
 	azureTokenCache = gocache.New(gocache.NoExpiration, 0)
+	gcpTokenCache = gocache.New(gocache.NoExpiration, 0)
 }
 
 // StoreToken stores a token in the cache
@@ -291,4 +295,105 @@ func (creds AzureWorkloadIdentityCreds) challengeAzureContainerRegistry(ctx cont
 	}
 
 	return tokenParams, nil
+}
+
+var _ Creds = GCPWorkloadIdentity{}
+
+type GCPWorkloadIdentity struct {
+	repoURL            string
+}
+
+func NewGCPWorkloadIdentityCreds(repoURL string) GCPWorkloadIdentity {
+	return GCPWorkloadIdentity{
+		repoURL:            repoURL,
+	}
+}
+
+func (creds GCPWorkloadIdentity) GetUsername() string {
+	return "oauth2accesstoken"
+}
+
+func (creds GCPWorkloadIdentity) GetPassword() (string, error) {
+	return creds.GetAccessToken()
+}
+
+func (creds GCPWorkloadIdentity) GetCAPath() string {
+	return ""
+}
+
+func (creds GCPWorkloadIdentity) GetCertData() []byte {
+	return nil
+}
+
+func (creds GCPWorkloadIdentity) GetKeyData() []byte {
+	return nil
+}
+
+func (creds GCPWorkloadIdentity) GetInsecureSkipVerify() bool {
+	return false
+}
+
+type gcpTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+func (creds GCPWorkloadIdentity) GetAccessToken() (string, error) {
+	registryHost := strings.Split(creds.repoURL, "/")[0]
+	ctx := context.Background()
+
+	// Compute hash as key for refresh token in the cache
+	key, err := argoutils.GenerateCacheKey("accesstoken-%s", registryHost)
+	if err != nil {
+		return "", fmt.Errorf("failed to compute key for cache: %w", err)
+	}
+
+	// Check cache for GitHub transport which helps fetch an API token
+	t, found := gcpTokenCache.Get(key)
+	if found {
+		fmt.Println("found gcp access token in cache")
+		return t.(string), nil
+	}
+
+	// Fetch token from the metadata server
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token", nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request to metadata server: %w", err)
+	}
+	req.Header.Add("Metadata-Flavor", "Google")
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to get token from metadata server: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("metadata server returned non-200 status: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body from metadata server: %w", err)
+	}
+
+
+	var tr gcpTokenResponse
+	err = json.Unmarshal(body, &tr)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal token response: %w", err)
+	}
+
+	tokenExpiry := time.Now().Add(time.Duration(tr.ExpiresIn) * time.Second)
+	cacheExpiry := workloadidentity.CalculateCacheExpiryBasedOnTokenExpiry(tokenExpiry)
+	if cacheExpiry > 0 {
+		gcpTokenCache.Set(key, tr.AccessToken, cacheExpiry)
+	}
+
+	return tr.AccessToken, nil
 }
